eb584f94a11d201bca3609731ffc95e27f140d54
Michael23123 <michaeljensen1207@gmail.com>
2025-11-23 19:26:49 +0100

    fixed things
    made it so the base enemy somewhat works

---
+++ global.gd
@@ -0,0 +1,15 @@
+extends Node
+
+var base_enemy
+
+class GameData:
+	var gold: int
+	var current_level: int
+
+var game_state_1: Game_Data = GameData()
+game_state_1.gold = 10
+game_state_1.current_level = 1
+
+var game_state_2: Game_Data = GameData()
+game_state_2.gold = 16
+game_state_2.current_level = 2
